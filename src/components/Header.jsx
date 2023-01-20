077e4e373ab076fdf35c9844e7acb99e03716193
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2023-01-20 10:09:44 +0200

    init

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import "../styles/Home.css"
+
+const Header = () => {
+  return (
+    <div className='hero-banner'>
+        <div className="content">
+            <Container>
+                <p>Hello there</p>
+            </Container>
+        </div>
+    </div>
+  );
+};
+
+export default Header;
