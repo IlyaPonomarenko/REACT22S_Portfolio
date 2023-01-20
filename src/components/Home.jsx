cd855f6227bf228231f4da94690cc08d27c48619
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2023-01-20 13:12:27 +0200

    updt

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import NavBar from "./NavBar";
-import "../styles/Home.css"
-import CardsCollection from "./CardsCollection"
-import Badge from 'react-bootstrap/Badge';
+import "../styles/Home.css";
+import CardsCollection from "./CardsCollection";
+import Badge from "react-bootstrap/Badge";
 
 const Home = () => {
   return (
     <div>
-      <NavBar/>
-      <h2>
+      <NavBar />
+      <h2 className="title">
+        Cards collection <Badge bg="secondary">New</Badge>
+      </h2>
+      <CardsCollection></CardsCollection>
+      <h2 className="title">
         Cards collection <Badge bg="secondary">New</Badge>
       </h2>
       <CardsCollection></CardsCollection>
